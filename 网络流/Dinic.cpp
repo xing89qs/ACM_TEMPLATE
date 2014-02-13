23e94a871658a7bf187da13a53cc35fa94eef70c
xing89qs <296750858@qq.com>
2014-02-13 19:39:21 +0800

    网络流

---
+++ 网络流/Dinic.cpp
@@ -0,0 +1,79 @@
+#define MAXN 100005
+#define MAXE 2000005
+
+int d[MAXN];
+int head[MAXN],nxt[MAXE],cur[MAXN];
+struct Edge{
+	int from,to,cap,flow;
+} e[MAXE];
+
+class Dinic{
+private:
+	int s,t,cnt;
+	int node;
+	bool bfs(){
+		for(int i = 0;i<node;i++) d[i] = -1;
+		d[s] = 0;
+		queue<int> q;
+		q.push(s);
+		while(!q.empty()){
+			int x = q.front();q.pop();
+			for(int i = head[x];~i;i = nxt[i]){
+				Edge &ed = e[i];
+				if(d[ed.to]==-1&&ed.cap>ed.flow){
+					d[ed.to] = d[x]+1;
+					q.push(ed.to);
+				}
+			}
+		}
+		return d[t]!=-1;
+	}
+	int dfs(int x,int a){
+		if(x==t||a==0) return a;
+		int flow = 0,f;
+		for(;~cur[x];cur[x] = nxt[cur[x]]){
+			Edge &ed = e[cur[x]];
+			if(d[ed.to]==d[x]+1&&(f = dfs(ed.to,min(ed.cap-ed.flow,a)))>0){
+				flow+=f;
+				ed.flow+=f;
+				e[cur[x]^1].flow-=f;
+				a-=f;
+				if(a==0) break;
+			}
+		}
+		return flow;
+	}
+	void addEdge(int from,int to,int cap){
+		e[cnt].from = from;
+		e[cnt].to = to;
+		e[cnt].cap = cap;
+		e[cnt].flow = 0;
+		int tmp = head[from];
+		head[from] = cnt;
+		nxt[cnt++] = tmp;
+		e[cnt].from = to;
+		e[cnt].to = from;
+		e[cnt].cap = 0;
+		e[cnt].flow = 0;
+		tmp = head[to];
+		head[to] = cnt;
+		nxt[cnt++] = tmp;
+	}
+public:
+	Dinic():cnt(0),node(0){
+	}
+	int maxFlow(){
+		int flow = 0;
+		while(bfs()){
+			for(int i = 0;i<node;i++) cur[i] = head[i];
+			flow+=dfs(s,INF);
+		}
+		return flow;
+	}
+
+	void buildGraph(){
+		s = node++;t = node++;
+		for(int i = 0;i<node;i++) head[i] = -1;
+	}
+};
+
