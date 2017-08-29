621ba632a4ade48c2b089543f857959b67c36cd8
Miraslov Klose <1273789365@qq.com>
2017-08-29 15:22:46 +0800

    暑假学习Modify

--- 计算几何/点与向量.cpp
+++ 计算几何/点与向量.cpp
@@ -1,22 +1,22 @@
 
 //两点距离的平方
-Type Norm(Vector v)
+Type SqrLength(Vector v)
 {
     return v.x*v.x+v.y*v.y;
 }
 
 //点在直线上的投影
-Point project(SegMent s,Point p)
+Point Project(SegMent s,Point p)
 {
     Vector x=s.b-s.a;
-    Type r=Dot(p-s.a,x)/Norm(x);
+    Type r=Dot(p-s.a,x)/SqrLength(x);
     return s.a+x*r;
 }
 
 //点关于直线的映象
-Point reflect(SegMent s,Point p)
+Point Reflect(SegMent s,Point p)
 {
-    return p+(project(s,p)-p)*2.0;
+    return p+(Project(s,p)-p)*2.0;
 }
 
 
@@ -27,7 +27,7 @@ static const int ONLINE_BACK = 2;   //v2相对v1反向
 static const int ONLINE_FRONT = -2; //v1、v2同向且v2比v1长
 static const int ON_SEGMENT = 0;    //v1、v2同向且v1比v2长
 
-int ccw(Point p0,Point p1,Point p2)
+int CCW(Point p0,Point p1,Point p2)
 {
     Vector a=p1-p0;
     Vector b=p2-p0;
@@ -44,9 +44,9 @@ int ccw(Point p0,Point p1,Point p2)
 }
 
 //判断两线段是否相交
-bool intersect(SegMent s1,SegMent s2)
+bool Intersect(SegMent s1,SegMent s2)
 {
-    if(ccw(s1.a,s1.b,s2.a)*ccw(s1.a,s1.b,s2.b)<=0 && ccw(s2.a,s2.b,s1.a)*ccw(s2.a,s2.b,s1.b)<=0)
+    if(CCW(s1.a,s1.b,s2.a)*CCW(s1.a,s1.b,s2.b)<=0 && CCW(s2.a,s2.b,s1.a)*CCW(s2.a,s2.b,s1.b)<=0)
         return true;
     return false;
 }
@@ -54,7 +54,7 @@ bool intersect(SegMent s1,SegMent s2)
 //两线段之间的距离
 Type DistanceBetweenTwoSegMent(SegMent s1,SegMent s2)
 {
-    if(intersect(s1,s2))
+    if(Intersect(s1,s2))
         return 0.0;
     return min(min(DistanceToSegMent(s1.a,s2),DistanceToSegMent(s1.b,s2)),min(DistanceToSegMent(s2.a,s1),DistanceToSegMent(s2.b,s1)));
 }
@@ -70,7 +70,7 @@ Point CrossPoint(SegMent s1,SegMent s2)
 }
 
 //点与多边形的关系（内包、在边上、在多边形外）
-int contains(Polygon g, Point p)
+int Contains(Polygon g, Point p)
 {
     int sz=g.v.size();
     bool flag=false;
