03621156dc1e574d3130d4b2f767cde06d1aed9e
Miraslov Klose <1273789365@qq.com>
2017-10-18 23:34:01 +0800

    备战区域赛——模板修改

--- 图论/2-SAT.cpp
+++ 图论/2-SAT.cpp
@@ -1,48 +1,155 @@
-class TwoSat{
+//2-SAT模板
+
+#define MAXN 10005
+#define MAXE 2000005
+
+class TwoSat
+{
 public:
-	int mark[MAXN<<1],s[MAXN<<1];
-	int c,cnt;
+    int head[MAXN];
+    struct Edge
+    {
+        int to,next;
+        Edge() {}
+        Edge(int to,int next):to(to),next(next) {}
+    } e[MAXE];
 
-	//使用前调用
-	void init(int n){
-		cnt = 0;
-		for(int i = 0;i<2*n;i++) head[i] = -1,mark[i] = 0;
-	}
+    int mark[MAXN<<1];  //mark[i<<1]==1，表示点i被选择；mark[i<<1|1]==1，表示点i没有被选择
+    int s[MAXN<<1];
+    int c,cnt,dfs_clock;
 
-	bool dfs(int x){
-		if(mark[x^1]) return false;
-		if(mark[x]) return true;
-		mark[x] = true;
-		s[c++] = x;
-		for(int i = head[x];~i;i = nxt[i]){
-			if(!dfs(e[i])) return false;
-		}
-		return true;
-	}
+    int dfn[MAXN<<1];
+    int low[MAXN<<1];
+    int belong[MAXN<<1];
+    int color[MAXN<<1]
 
-	inline void addEdge(int u,int v){
-		e[cnt] = v;
-		int tmp = head[u];
-		head[u] = cnt;
-		nxt[cnt++] = tmp;
-	}
+    //使用前调用
+    void init(int n)
+    {
+        cnt = 0;
+        dfs_clock = 0;
+        for(int i = 0; i<2*n; i++) head[i] = -1,mark[i] = 0;
+    }
 
-	//n是变量数
-	bool solve(int n){
-		for(int i = 0;i<n*2;i+=2){
-			if(!mark[i]&&!mark[i^1]){
-				c = 0;
-				if(!dfs(i)){
-					while(c>0) mark[s[--c]] = false;
-					if(!dfs(i^1)) return false;
-				}
-			}
-		}
-		return true;
-	}
+    bool dfs(int u) //用来判断当前的强连通分量当中会不会出现矛盾
+    {
 
-	void buildGraph(int n){
-		init(n);
-	}
+        if(mark[u^1]) return false; //如果需要被选的不能被选那么矛盾
+        if(mark[u]) return true;    //如果需要被选的已经被选，那么当前联通分量一定    不会出现矛盾
+        mark[u] = true; //如果当前点需要被选，那么选上它，并且标记
+        s[c++] = u; //当前的强连通分量加上这个点
+        //找到与当前点相连点，判断他们的状态
+        for(int i = head[u]; ~i; i = e[i].next)
+        {
+            int v = e[i].to;
+            if(!dfs(v))
+                return false;
+        }
+        return true;
+    }
+
+    inline void addEdge(int u,int v)
+    {
+        e[cnt].to = v;
+        e[cnt].next = head[u];
+        head[u] = cnt++;
+    }
+
+    void tarjan(int u)
+    {
+        dfn[u] = low[u] = ++dfs_clock;
+        mark[u] = 1;
+        s.push( u);
+        for(int i = head[u]; ~i; i = e[i].next)
+        {
+            int v = e[i].to;
+            if(!mark[v])
+            {
+                tarjan(v);
+                low[u] = min(low[u],low[v]);
+            }
+            if(mark[v]==1)
+                low[u] = min(low[u],dfn[v]);
+        }
+        if(dfn[u]==low[u])
+        {
+            int temp;
+            do
+            {
+                temp = s.top();
+                belong[temp] = cnt;
+                mark[temp] = 2;
+                s.pop();
+            }
+            while(temp!=u);
+            cnt++;
+        }
+    }
+
+    bool check()    //利用强连通缩点判断2-SAT问题是否有解
+    {
+        for(int i = 0; i<2*n; i++)
+        {
+            if(!mark[i])
+                tarjan(i);
+        }
+        for(int i = 0; i<n; i++)
+        {
+            if(belong[i<<1]==belong[i<<1|1])
+                return false;
+        }
+        return true;
+    }
+
+    void topoSort() //按照拓扑序求得任意一组解
+    {
+        queue<int> q;
+        for(int i = 1; i<t; i++)
+        {
+            if(!indegree[i])
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            if(!color[u])
+            {
+                color[u] = 1;
+                color[conflict[u]] = 2; //conflict数组记录的是与当前点冲突的点
+            }
+            for(int i = head[u]; ~i; i = e[i].next)
+            {
+                int v = e[i].to;
+                indegree[v]--;
+                if(!indegree[v])
+                    q.push(v);
+            }
+        }
+    }
+
+    //n是变量数
+    bool solve(int n)   //暴力求字典序最小的解（复杂度O(n*m)）
+    {
+        for(int i = 0; i<n*2; i+=2)
+        {
+            if(!mark[i]&&!mark[i^1])
+            {
+                c = 0;
+                if(!dfs(i)) //如果矛盾，那么这个强连通分量里的点都不能选取
+                {
+                    while(c>0)
+                        mark[s[--c]] = false;
+                    if(!dfs(i^1))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void buildGraph(int n)
+    {
+        init(n);
+    }
 } twosat;
-
