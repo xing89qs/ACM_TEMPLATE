6f80802fd17f8e10588f1d0d1fce0b53455e296b
Miraslov Klose <1273789365@qq.com>
2017-11-05 14:51:47 +0800

    备战18省赛——模板修改

--- 图论/边-双联通分量.cpp
+++ 图论/边-双联通分量.cpp
@@ -1,54 +1,94 @@
-class BCC{
+//边-双联通分量（桥）
+
+#define MAXN 1005
+#define MAXE 2000005
+
+class BCC
+{
 public:
-	int bridge[MAXE];
-	int pre[MAXN],belong[MAXN],bcc_cnt;
-	int dfs_clock;
+    int head[MAXN];
+    struct Edge
+    {
+        int to,next;
+        Edge() {}
+        Edge(int to,int next):to(to),next(next) {}
+    } e[MAXE];
 
-	int tarjan(int u,int fa){
-		int lowu = pre[u] = ++dfs_clock;
-		for(int i = head[u];~i;i = nxt[i]){
-			if(i==fa) continue;
-			int v = e[i];
-			if(!pre[v]){
-				int lowv = tarjan(v,i^1);
-				lowu = min(lowu,lowv);
-				if(lowv>pre[u]){
-					bridge[i] = bridge[i^1] = true;
-				}
-			}else if(pre[v]<pre[u]){
-				lowu = min(lowu,pre[v]);
-			}
-		}
-		return lowu;
-	}
+    int bridge[MAXE];
+    int dfn[MAXN];
+    int low[MAXN];
+    int belong[MAXN];
+    int cnt;
+    int bcc_cnt;
+    int dfs_clock;
 
-	void dfs(int u,int fa){
-		belong[u] = bcc_cnt;
-		for(int i = head[u];~i;i = nxt[i]){
-			if(i==fa) continue;
-			int v = e[i];
-			if(!bridge[i]&&!belong[v]){
-				dfs(v,i^1);
-			}
-		}
-	}
+    void init()
+    {
+        cnt = 0;
+        memset(head,-1,sizeof(head));
+        memset(dfn,0,sizeof(dfn));
+    }
 
-	//找边-双连通分量
-	void find_bcc(int n){
-		for(int i = 0;i<n;i++) pre[i] = 0;
-		//cnt是边数
-		for(int i = 0;i<cnt;i++) bridge[i] = false;
-		dfs_clock = bcc_cnt = 0;
-		for(int i = 0;i<n;i++){
-			if(!pre[i]) tarjan(i,-1);
-			belong[i] = 0;
-		}
-		//belong表示所属的边联通分量的id
-		for(int i = 0;i<n;i++){
-			if(!belong[i]){
-				bcc_cnt++;
-				dfs(i,-1);
-			}
-		}
-	}
-};
+    void addEdge(int u,int v)
+    {
+        e[cnt].to = v;
+        e[cnt].next = head[u];
+        head[u] = cnt++;
+    }
+
+    void tarjan(int u,int fa)
+    {
+        low[u] = dfn[u] = ++dfs_clock;
+        for(int i = head[u]; ~i; i = e[i].next)
+        {
+            if(v==fa) continue;
+            int v = e[i].to;
+            if(!dfn[v])
+            {
+                tarjan(v,u);
+                low[u] = min(low[u],low[v]);
+                if(low[v]>dfn[u])
+                    bridge[i] = bridge[i^1] = true;
+            }
+            else if(dfn[v]<dfn[u])
+                low[u] = min(low[u],dfn[v]);
+        }
+    }
+
+    void dfs(int u,int fa)
+    {
+        belong[u] = bcc_cnt;
+        for(int i = head[u]; ~i; i = e[i].next)
+        {
+            if(i==fa) continue;
+            int v = e[i].to;
+            if(!bridge[i]&&!belong[v])
+                dfs(v,i^1);
+        }
+    }
+
+    //找边-双连通分量
+    void find_bcc(int n)
+    {
+        for(int i = 0; i<n; i++)
+            dfn[i] = 0;
+        for(int i = 0; i<cnt; i++)  //cnt是边数
+            bridge[i] = false;
+        dfs_clock = bcc_cnt = 0;
+        for(int i = 0; i<n; i++)
+        {
+            if(!dfn[i])
+                tarjan(i,-1);
+            belong[i] = 0;
+        }
+        //belong表示所属的边联通分量的id
+        for(int i = 0; i<n; i++)
+        {
+            if(!belong[i])
+            {
+                bcc_cnt++;
+                dfs(i,-1);
+            }
+        }
+    }
+} bcc;
