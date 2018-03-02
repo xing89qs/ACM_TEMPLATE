28bb1a64895f89fc4168c26bb1657391adfb1916
Miraslov Klose <1273789365@qq.com>
2018-03-02 23:22:52 +0800

    备战18蓝桥杯——模板修改

--- 字符串/Manacher.cpp
+++ 字符串/Manacher.cpp
@@ -23,6 +23,7 @@ int manacher(char *str)
         return -1;
     int mx = 0,id = 0;  //mx为以newstr[id]为中心的最长回文串的右边界，即mx=id+rad[id]
     //int axis = -1;
+    //int l,r;  //最大回文串的左右边界
     for(int i=1; i<=j; i++)
     {
         if(mx > i)
@@ -36,6 +37,8 @@ int manacher(char *str)
         {
             Max = rad[i];
             axis = i;   //记录最大回文串对称中心（对称轴）
+            l = (i - rad[i]) / 2;
+            r = (i + rad[i]) / 2 - 2;
         }
         */
         if(rad[i] + i > mx) //若新得到的回文串右端点位置大于mx，则要更新id和mx的值
