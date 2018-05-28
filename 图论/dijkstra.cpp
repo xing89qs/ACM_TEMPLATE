c6ea93209d0496785b9b0b755926ac8b3a7016ee
Miraslov Klose <1273789365@qq.com>
2018-05-28 14:54:01 +0800

    退役后模板修正

--- 图论/dijkstra.cpp
+++ 图论/dijkstra.cpp
@@ -3,55 +3,69 @@
 
 typedef int Type;
 
-class Dijkstra{
+class Dijkstra
+{
 public:
-	int head[MAXN],nxt[MAXE],cnt;
-	Type d[MAXN];
-	struct Edge{
-		int v;
-		Type cost;
-		Edge(){}
-		Edge(int v,Type cost):v(v),cost(cost){}
-		bool operator<(const Edge& ed) const{
-			return cost>ed.cost;
-		}
-	} e[MAXE];	
-	bool vis[MAXN];
+    int head[MAXN],nxt[MAXE],cnt;
+    Type d[MAXN];
+    struct Edge
+    {
+        int v;
+        Type cost;
+        Edge() {}
+        Edge(int v,Type cost):v(v),cost(cost) {}
+        bool operator<(const Edge& ed) const
+        {
+            return cost>ed.cost;
+        }
+    } e[MAXE];
+    bool vis[MAXN];
 
-	//初始化，n为点数
-	void init(int n){
-		cnt = 0;
-		for(int i = 0;i<n;i++) head[i] = -1;
-	}
+    //初始化，n为点数
+    void init(int n)
+    {
+        cnt = 0;
+        for(int i = 0; i<n; i++)
+            head[i] = -1;
+    }
 
-	inline void addEdge(int u,int v,Type cost){
-		e[cnt] = Edge(v,cost);
-		int tmp = head[u];
-		head[u] = cnt;
-		nxt[cnt++] = tmp;	
-	}
+    inline void addEdge(int u,int v,Type cost)
+    {
+        e[cnt] = Edge(v,cost);
+        int tmp = head[u];
+        head[u] = cnt;
+        nxt[cnt++] = tmp;
+    }
 
-	Type dijkstra(int s,int t,int n){
-		for(int i = 0;i<n;i++) d[i] = INF,vis[i] = false;
-		d[s] = 0;
-		priority_queue<Edge> q;
-		q.push(Edge(s,0));
-		while(!q.empty()){
-			Edge ed = q.top();q.pop();
-			vis[ed.v] = true;
-			if(ed.v==t) return d[t];
-			for(int i = head[ed.v];~i;i = nxt[i]){
-				Edge ee = e[i];
-				if(!vis[ee.v]&&d[ee.v]>d[ed.v]+ee.cost){
-					d[ee.v] = d[ed.v]+ee.cost;
-					/*
-					 *  最短路树
-						p[ee.v] = MP(u,i);
-					*/
-					q.push(Edge(ee.v,d[ee.v]));
-				}
-			}
-		}
-		return d[t];
-	}
+    Type dijkstra(int s,int t,int n)
+    //void dijkstra(int s,int n)
+    {
+        for(int i = 0; i<n; i++)
+            d[i] = INF,vis[i] = false;
+        d[s] = 0;
+        priority_queue<Edge> q;
+        q.push(Edge(s,0));
+        while(!q.empty())
+        {
+            Edge ed = q.top();
+            q.pop();
+            vis[ed.v] = true;
+            if(ed.v==t) //如果未给出终点t，则完全可以不要跟t有关的语句
+                return d[t];
+            for(int i = head[ed.v]; ~i; i = nxt[i])
+            {
+                Edge ee = e[i];
+                if(!vis[ee.v]&&d[ee.v]>d[ed.v]+ee.cost)
+                {
+                    d[ee.v] = d[ed.v]+ee.cost;
+                    /*
+                     *  最短路树
+                    	p[ee.v] = MP(u,i);
+                    */
+                    q.push(Edge(ee.v,d[ee.v]));
+                }
+            }
+        }
+        return d[t];
+    }
 } dij;
