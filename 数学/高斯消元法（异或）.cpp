5a98fe106f5893215c699c311b863f5541aed1b5
Miraslov Klose <1273789365@qq.com>
2019-05-04 12:04:27 +0800

    修改（2019.05）

--- 数学/高斯消元法（异或）.cpp
+++ 数学/高斯消元法（异或）.cpp
@@ -17,7 +17,7 @@ int Gauss_xor(int equ, int var) //equ为方程数，var为未知数个数
     for(k = 0, col = 0; k < equ && col < var; k++, col++)
     {
         int max_r = k;
-        for(int i = k + 1; i < equ; i++)
+        for(int i = k+1; i < equ; i++)
         {
             if(abs(a[i][col]) > abs(a[max_r][col])) //记录第一个第col列系数非零的行
                 max_r = i;
@@ -30,14 +30,14 @@ int Gauss_xor(int equ, int var) //equ为方程数，var为未知数个数
         }
         if(max_r != k)
         {
-            for(int j = col; j < var + 1; j++)    //将第一个第col列系数非零的行换到第k行
+            for(int j = col; j < var+1; j++)    //将第一个第col列系数非零的行换到第k行
                 swap(a[k][j],a[max_r][j]);
         }
-        for(int i = k + 1; i < equ; i++)    //开始消元
+        for(int i = k+1; i < equ; i++)    //开始消元
         {
             if(a[i][col])   //系数非零
             {
-                for(int j = col; j < var + 1; j++)    //从当前第col列开始消元
+                for(int j = col; j < var+1; j++)    //从当前第col列开始消元
                     a[i][j] ^= a[k][j];
             }
         }
@@ -52,11 +52,57 @@ int Gauss_xor(int equ, int var) //equ为方程数，var为未知数个数
     if(k < var) //
         return var - k; //返回自由变元个数
     /*有唯一解*/
-    for(int i = var - 1; i >= 0; i--)
+    for(int i = var-1; i >= 0; i--)
     {
         x[i] = a[i][var];
-        for(int j = i + 1; j < var; j++)  //回代求出解集
+        for(int j = i+1; j < var; j++)  //回代求出解集
             x[i] ^= (a[i][j] && x[j]);
     }
     return 0;
+}
+
+int solve(int equ, int var) //枚举自由变元
+{
+    int res = Gauss_xor(equ,var);
+    if(res == -1)   //无解
+        return -1;
+    else if(res == 0)   //唯一解
+    {
+        int ans = 0;
+        for(int i = 0; i < var; i++)
+            ans += x[i];
+        return ans;
+    }
+    else    //多解（枚举自由变元）
+    {
+        int ans = INF;
+        for(int i = 0; i < (1<<res); i++)   //枚举所有状态
+        {
+            int cnt = 0;   //当前涂色方案需要涂的次数
+            for(int j = 0; j < res; j++)  //枚举自由变元
+            {
+                x[free_x[j]] = (i & (1<<j));  //该自由变元取1
+                if(x[free_x[j]])
+                    cnt++;
+            }
+            for(int j = var-res-1; j >= 0; j--)   //从消完元矩阵的主对角线非0的最后一行开始回代
+            {
+                int idx;
+                for(idx = j; idx < var; idx++)
+                {
+                    if(a[j][idx])
+                        break;
+                }
+                x[idx] = a[j][var];
+                for(int l = idx+1; l < var; l++)  //回代求出解集
+                {
+                    if(a[j][l])
+                        x[idx] ^= x[l];
+                }
+                cnt += x[idx];
+            }
+            ans = min(ans,cnt);
+        }
+        return ans;
+    }
 }
