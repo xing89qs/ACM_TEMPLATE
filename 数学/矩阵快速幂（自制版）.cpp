b3f883edb5cb2a221a9fb425e2f4d24c07a1665c
ThoseBygones <1273789365@qq.com>
2020-11-11 10:39:49 +0800

    修改（2020.11）

--- 数学/矩阵快速幂（自制版）.cpp
+++ 数学/矩阵快速幂（自制版）.cpp
@@ -12,76 +12,80 @@ Type mod;
 struct Matrix   //矩阵的类
 {
     Type x[MAXN][MAXN];
-    void init() //将其初始化为单位矩阵
+    void init()
     {
-        memset(x,0,sizeof(x));
-        for(int i=0; i<n; i++)
-            x[i][i]=1;
+        memset(x, 0, sizeof(x));
+    }
+    void toIdentity() //初始化为单位矩阵
+    {
+        memset(x, 0, sizeof(x));
+        for(int i = 0; i < n; i++)
+            x[i][i] = 1;
     }
 } A;
 
-Matrix mul(Matrix a,Matrix b)   //(a*b)%mod 矩阵乘法
+Matrix mul(Matrix a, Matrix b)   //(a*b)%mod 矩阵乘法
 {
     Matrix ans;
-    for(int i=0; i<n; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j = 0; j < n; j++)
         {
-            ans.x[i][j]=0;
-            for(int k=0; k<n; k++)
-                ans.x[i][j]+=a.x[i][k]*b.x[k][j];
-            ans.x[i][j]%=mod;
+            ans.x[i][j] = 0;
+            for(int k = 0; k < n; k++)
+                ans.x[i][j] += a.x[i][k] * b.x[k][j];
+            ans.x[i][j] %= mod;
         }
     }
     return ans;
 }
 
-Matrix add(Matrix a,Matrix b)   //(a+b)%mod 矩阵加法
+Matrix add(Matrix a, Matrix b)   //(a+b)%mod 矩阵加法
 {
-    int i,j;
+    int i, j;
     Matrix ans;
-    for(i=0; i<n; i++)
+    for(i = 0; i < n; i++)
     {
-        for(j=0; j<n; j++)
+        for(j = 0; j < n; j++)
         {
-            ans.x[i][j]=a.x[i][j]+b.x[i][j];
-            ans.x[i][j]%=mod;
+            ans.x[i][j] = a.x[i][j] + b.x[i][j];
+            ans.x[i][j] %= mod;
         }
     }
     return ans;
 }
 
-Matrix pow(Matrix a,Type k)  //(a^n)%mod 矩阵快速幂
+Matrix pow(Matrix a, Type k)  //(a^n)%mod 矩阵快速幂
 {
     Matrix ans;
-    ans.init();
+    ans.toIdentity();
     while(k)
     {
-        if(k&1) //k%2
-            ans=mul(ans,a);
-        k/=2;
-        a=mul(a,a);
+        if(k & 1) //k%2
+            ans = mul(ans, a);
+        k /= 2;
+        a = mul(a, a);
     }
     return ans;
 }
 
-Matrix sum(Matrix a,Type k)  //(a+a^2+a^3....+a^n)%mod 矩阵的幂和
+Matrix sum(Matrix a, Type k)  //(a+a^2+a^3....+a^n)%mod 矩阵的幂和
 {
     Type m;
-    Matrix ans,pre;
-    if(k==1)
+    Matrix ans, pre;
+    if(k == 1)
         return a;
-    m=k/2;
-    pre=sum(a,m);   //[1,n/2]
-    ans=add(pre,mul(pre,pow(a,m))); //ans=[1,n/2]+a^(n/2)*[1,n/2]
-    if(n&1)
-        ans=add(ans,pow(a,k));  //ans=ans+a^n
+    m = k / 2;
+    pre = sum(a, m);   //[1,n/2]
+    ans = add(pre, mul(pre, pow(a, m))); //ans=[1,n/2]+a^(n/2)*[1,n/2]
+    if(n & 1)
+        ans = add(ans, pow(a, k));  //ans=ans+a^n
     return ans;
 }
 
 void print(Matrix a)    //输出矩阵
 {
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            printf("%lld%c",a.x[i][j],j==n-1?'\n':' ');
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            printf("%lld%c", a.x[i][j], j == n - 1 ? '\n' : ' ');
 }
