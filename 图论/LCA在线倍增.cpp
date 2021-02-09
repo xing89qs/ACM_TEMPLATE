3c029710d793469723b9718d05654842e94d98fe
ThoseBygones <1273789365@qq.com>
2021-02-09 21:41:06 +0800

    更新（2021.02）

--- 图论/LCA在线倍增.cpp
+++ 图论/LCA在线倍增.cpp
@@ -1,24 +1,23 @@
 //LCA在线倍增（需要<cmath>头文件）
 
-#define MAXN 100005
-#define MAXE 200005
+const int MAXN = 100005;
+const int MAXE = 200005;
 
 class LCA_Online_Multiplication
 {
 public:
 
-
-    int head[MAXN],nxt[MAXE],e[MAXE];
+    int head[MAXN], nxt[MAXE], e[MAXE];
     int cnt;
 
     void init(int n)
     {
         this -> n = n;
         cnt = 0;
-        memset(head,-1,sizeof(head));
+        memset(head, -1, sizeof(head));
     }
 
-    inline void addEdge(int u,int v)
+    inline void addEdge(int u, int v)
     {
         e[cnt] = v;
         int tmp = head[u];
@@ -30,63 +29,71 @@ public:
     int p[MAXN][20];
     int n;
 
-    void dfs(int u,int fa,int d)
+    void dfs(int u, int fa, int d)
     {
         dep[u] = d;
         p[u][0] = fa;
-        for(int i = 1; (1<<i)<=n; i++)
+        for(int i = 1; (1 << i) <= n; i++)
         {
-            if(~p[u][i-1]) p[u][i] = p[p[u][i-1]][i-1];
-            else p[u][i] = -1;
+            if(~p[u][i - 1])
+                p[u][i] = p[p[u][i - 1]][i - 1];
+            else
+                p[u][i] = -1;
         }
         for(int i = head[u]; ~i; i = nxt[i])
         {
             int v = e[i];
-            if(v==fa) continue;
-            dfs(v,u,d+1);
+            if(v == fa)
+                continue;
+            dfs(v, u, d + 1);
         }
     }
 
-    int LCA(int u,int v)
+    int LCA(int u, int v)
     {
-        if(dep[u]>dep[v]) swap(u,v);
-        if(dep[u]<dep[v])
+        if(dep[u] > dep[v])
+            swap(u, v);
+        if(dep[u] < dep[v])
         {
             int offset = dep[v] - dep[u];
-            for(int i = 0; (1<<i)<=n; i++) if(offset&(1<<i)) v = p[v][i];
-        }
-        if(u!=v)
-        {
-            for(int i = log2(n); i>=0; i--)
+            for(int i = 0; (1 << i) <= n; i++)
             {
-                if(p[u][i]!=p[v][i])
-                    u = p[u][i],v = p[v][i];
+                if(offset & (1 << i))
+                    v = p[v][i];
             }
-            u = p[u][0],v = p[v][0];
+        }
+        if(u != v)
+        {
+            for(int i = log2(n); i >= 0; i--)
+            {
+                if(p[u][i] != p[v][i])
+                    u = p[u][i], v = p[v][i];
+            }
+            u = p[u][0], v = p[v][0];
         }
         return u;
     }
 
     //求树上两点距离
-    int treeDis(int u,int v)
+    int treeDis(int u, int v)
     {
-        return dep[u] + dep[v] - 2 * dep[LCA(u,v)];
+        return dep[u] + dep[v] - 2 * dep[LCA(u, v)];
     }
 
     void buildGraph()
     {
-        for(int i = 1; i<n; i++)
+        for(int i = 1; i < n; i++)
         {
-            int u,v;
-            scanf("%d %d",&u,&v);
-            addEdge(u,v);
-            addEdge(v,u);
+            int u, v;
+            scanf("%d%d", &u, &v);
+            addEdge(u, v);
+            addEdge(v, u);
         }
     }
 
     void solve()
     {
         buildGraph();
-        dfs(1,-1,0);
+        dfs(1, -1, 0);
     }
 } lca_multi;
