acccb5c9ca9be42a9f200c3ebe9f51281a1c5a26
ThoseBygones <1273789365@qq.com>
2021-06-08 23:27:13 +0800

    更新（2021.06）

--- 常用小算法汇总/最长上升子序列（LIS）.cpp
+++ 常用小算法汇总/最长上升子序列（LIS）.cpp
@@ -4,8 +4,9 @@ int dp[MAXN];
 
 void LIS()
 {
-    fill(dp,dp+n,INF);
-    for(int i=0; i<n; i++)
-        *lower_bound(dp,dp+n,a[i]) = a[i];
-    printf("%d\n",lower_bound(dp,dp+n,INF)-dp);
+    fill(dp, dp + n,INF);
+    for(int i = 0; i < n; i++)
+        *lower_bound(dp, dp + n, a[i]) = a[i];  //最长上升子序列
+        //*upper_bound(dp, dp + n, a[i]) = a[i];    //最长不下降子序列
+    printf("%d\n", lower_bound(dp, dp + n, INF) - dp);
 }
