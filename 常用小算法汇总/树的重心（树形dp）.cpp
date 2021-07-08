d0e8c5464066dca427875fe239ad4eb6f1e77247
ThoseBygones <1273789365@qq.com>
2021-07-08 10:03:09 +0800

    修改（2021.07）

--- 常用小算法汇总/树的重心（树形dp）.cpp
+++ 常用小算法汇总/树的重心（树形dp）.cpp
@@ -2,23 +2,25 @@
 //建边的时候要建双向边！
 #define MAXN 50010
 
-int num[MAXN];  //以每个结点为根子树的结点数（包含自身）
+int sz[MAXN];  //以每个结点为根子树的结点数（包含自身）
 int dp[MAXN];   //dp[i]表示沿树路径到第i个点时子树结点数的最大值（不含自身）
-int n;
+int nodecnt;    //当前子树中所有结点数
+int rt; //求得当前子树的重心
 
 void dfs(int u, int fa)
 {
-    dp[u] = 0;
-    num[u] = 1;
-    for(int i=head[u]; ~i; i=e[i].nxt)
+    dp[u] = 0, sz[u] = 1;
+    for(int i = head[u]; ~i; i = e[i].nxt)
     {
         int v = e[i].to;
         if(v != fa)
         {
             dfs(v, u);
-            num[u] += num[v];
-            dp[u] = max(dp[u], num[v]);
+            sz[u] += sz[v];
+            dp[u] = max(dp[u], sz[v]);
         }
     }
-    dp[u] = max(dp[u], n - num[u]);
+    dp[u] = max(dp[u], nodecnt - sz[u]);      //树的重心的定义：以该点为根的有根树，最大子树的结点数最小
+    if(dp[u] < dp[rt])
+        rt = u;
 }
