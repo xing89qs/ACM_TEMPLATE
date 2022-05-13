5ef61c97d1ef64735f2cbfb99e224dec4eed8f5e
ThoseBygones <1273789365@qq.com>
2022-05-13 17:46:32 +0800

    修改（2022.05）

--- 数学/逆元.cpp
+++ 数学/逆元.cpp
@@ -1,8 +1,15 @@
-//求逆元
 
-int modInv(int a,int m)
+/** 扩展欧几里得算法求逆元 **/
+int modInv(int a, int m)
 {
-    int x,y;
-    exgcd(a,m,x,y);
+    int x, y;
+    exgcd(a, m, x, y);
     return (m + x % m) % m;
+}
+
+
+/** 费马小定理 / 欧拉定理求逆元 **/
+LL modInv(LL x, LL mod)
+{
+    return quickPow(x, mod - 2, mod);
 }
